ed6b2de1a41ec61a821052f5a130c557d4686fb2
Ikhsan <ikhsank93@gmail.com>
2022-12-18 17:59:52 +0700

    update database

--- manajemen_gaji.sql
+++ manajemen_gaji.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 11, 2022 at 07:47 AM
+-- Generation Time: Dec 18, 2022 at 11:59 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -89,7 +89,8 @@ CREATE TABLE `karyawan` (
 
 INSERT INTO `karyawan` (`id_karyawan`, `id_jabatan`, `nik`, `nama_karyawan`, `jenis_kelamin`, `alamat`, `tanggal_lahir`) VALUES
 (1, 7, '3305847586732229', 'Ikhsan Kurniawan', 'laki-laki', 'Kebumen', '2003-05-11'),
-(2, 5, '3304758695867112', 'Rian Kusdiono', 'laki-laki', 'Sokaraja', '2002-05-06');
+(2, 5, '3304758695867112', 'Rian Kusdiono', 'laki-laki', 'Sokaraja', '2002-05-06'),
+(3, 8, '08977867676667', 'Ammar Daffa', 'laki-laki', 'Purowketo', '2022-10-03');
 
 -- --------------------------------------------------------
 
@@ -100,16 +101,17 @@ INSERT INTO `karyawan` (`id_karyawan`, `id_jabatan`, `nik`, `nama_karyawan`, `je
 CREATE TABLE `master_gaji_pokok` (
   `id_master` int(11) NOT NULL,
   `nama_master` varchar(200) NOT NULL,
-  `gaji_master` float NOT NULL
+  `gaji_master` float NOT NULL,
+  `persen_pajak` varchar(200) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `master_gaji_pokok`
 --
 
-INSERT INTO `master_gaji_pokok` (`id_master`, `nama_master`, `gaji_master`) VALUES
-(6, 'Master Gaji Backend Programmer', 5000000),
-(7, 'Master Gaji Fullstrack Programmer', 6000000);
+INSERT INTO `master_gaji_pokok` (`id_master`, `nama_master`, `gaji_master`, `persen_pajak`) VALUES
+(7, 'Master Gaji Fullstrack Programmer', 6000000, '5'),
+(9, 'Master Gaji Frontend Programmer', 5100000, '11');
 
 -- --------------------------------------------------------
 
@@ -122,8 +124,10 @@ CREATE TABLE `transaksi_gaji` (
   `id_karyawan` int(11) NOT NULL,
   `id_master` int(11) NOT NULL,
   `waktu_gaji` varchar(200) NOT NULL,
-  `bonus_gaji` int(11) NOT NULL,
-  `nominal_gaji` int(11) NOT NULL,
+  `bonus_gaji` float NOT NULL,
+  `nominal_gaji` float NOT NULL,
+  `persen_pajak` varchar(200) NOT NULL,
+  `potongan_pajak` float NOT NULL,
   `keterangan` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -131,9 +135,8 @@ CREATE TABLE `transaksi_gaji` (
 -- Dumping data for table `transaksi_gaji`
 --
 
-INSERT INTO `transaksi_gaji` (`id_transaksi`, `id_karyawan`, `id_master`, `waktu_gaji`, `bonus_gaji`, `nominal_gaji`, `keterangan`) VALUES
-(14, 1, 6, '2022-11', 1000000, 5000000, 'Gaji bulan november 2022'),
-(15, 1, 6, '2022-12', 1500000, 5000000, 'Gaji bulan desember 2022');
+INSERT INTO `transaksi_gaji` (`id_transaksi`, `id_karyawan`, `id_master`, `waktu_gaji`, `bonus_gaji`, `nominal_gaji`, `persen_pajak`, `potongan_pajak`, `keterangan`) VALUES
+(20, 1, 9, '2022-10', 2000000, 5100000, '11', 561000, 'gaji oktober');
 
 --
 -- Indexes for dumped tables
@@ -169,8 +172,7 @@ ALTER TABLE `master_gaji_pokok`
 --
 ALTER TABLE `transaksi_gaji`
   ADD PRIMARY KEY (`id_transaksi`),
-  ADD KEY `id_karyawan` (`id_karyawan`,`id_master`),
-  ADD KEY `id_master` (`id_master`);
+  ADD KEY `id_karyawan` (`id_karyawan`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -186,25 +188,25 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `jabatan`
 --
 ALTER TABLE `jabatan`
-  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `karyawan`
 --
 ALTER TABLE `karyawan`
-  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_karyawan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `master_gaji_pokok`
 --
 ALTER TABLE `master_gaji_pokok`
-  MODIFY `id_master` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_master` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `transaksi_gaji`
 --
 ALTER TABLE `transaksi_gaji`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
@@ -220,8 +222,7 @@ ALTER TABLE `karyawan`
 -- Constraints for table `transaksi_gaji`
 --
 ALTER TABLE `transaksi_gaji`
-  ADD CONSTRAINT `transaksi_gaji_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaksi_gaji_ibfk_2` FOREIGN KEY (`id_master`) REFERENCES `master_gaji_pokok` (`id_master`) ON UPDATE CASCADE;
+  ADD CONSTRAINT `transaksi_gaji_ibfk_1` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id_karyawan`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
